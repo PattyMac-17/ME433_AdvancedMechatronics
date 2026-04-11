7bc63e96ed088a1faded612fa5aa335021006d04
Patrick McLean <patrickmclean2027@u.northwestern.edu>
2026-04-11 02:25:48 -0500

    2:24am and i have no idea why blue won't light. at least I can write without hanging the program forever

--- HW3/HW3_Code/HW3_Code.c
+++ HW3/HW3_Code/HW3_Code.c
@@ -21,10 +21,13 @@
 //step 2: read button on i2c chip GP0 and control GP7 led
 
 #define I2C_PORT i2c0
-#define I2C_SDA 6
-#define I2C_SCL 7
-#define picoHeart 15
-#define ADDRESS 0x20
+#define I2C_SDA 4
+#define I2C_SCL 5
+#define PICOHEART 15
+//#define ADDRESS_READ 0b01000001 didnt work
+//#define ADDRESS_WRITE 0b01000000 didnt work
+#define ADDRESS 0b0100000
+#define MCP_RESET 16 //yes im adding the reset pin for sanity
 
 //prototypes
 void setPin(uint8_t addr, uint8_t reg, uint8_t val){
@@ -32,21 +35,29 @@ void setPin(uint8_t addr, uint8_t reg, uint8_t val){
     buf[0] = reg;
     buf[1] = val;
     i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    return;
 }
 unsigned char readPin(uint8_t addr, uint8_t reg, uint8_t val);
 
-void initializeChip(){
+void initializeChip(uint8_t addr){
     //IODIR = 0x00
     uint8_t buf1[2];
     buf1[0] = 0x00;
-    buf1[1] = 0b00000001;
-    i2c_write_blocking(i2c_default, ADDRESS, buf1, 2, false);
+    buf1[1] = 0b00000000;
+    i2c_write_blocking(i2c_default, addr, buf1, 2, false);
 
     //OLAT = 0x0A, set all LOW
     uint8_t buf2[2];
     buf2[0] = 0x0A;
     buf2[1] = 0b00000000;
-    i2c_write_blocking(i2c_default, ADDRESS, buf2, 2, false);
+    i2c_write_blocking(i2c_default, addr, buf2, 2, false);
+
+    //IPOL = 0x01
+    uint8_t buf3[2];
+    buf3[0] = 0x01;
+    buf3[1] = 0b00000000;
+    i2c_write_blocking(i2c_default, addr, buf3, 2, false);
+    return;
 }
 
 int main()
@@ -58,17 +69,38 @@ int main()
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_init(15);
-    gpio_set_dir(15, GPIO_OUT);
+
+    gpio_init(PICOHEART);
+    gpio_set_dir(PICOHEART, GPIO_OUT);
+
+    gpio_init(MCP_RESET);
+    gpio_set_dir(MCP_RESET, GPIO_OUT);
+
+    gpio_put(MCP_RESET, 0);
+    sleep_ms(100);
+    gpio_put(MCP_RESET, 1);
+    sleep_ms(100);
+
+    gpio_put(PICOHEART, 0);
+    sleep_ms(100);
+    gpio_put(PICOHEART, 1);
+    sleep_ms(100);
+    gpio_put(PICOHEART, 0);
+    sleep_ms(100);
+    gpio_put(PICOHEART, 1);
+    sleep_ms(100);
+    gpio_put(PICOHEART, 0);
+    sleep_ms(100);
+
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    initializeChip();
+    initializeChip(ADDRESS);
     sleep_ms(100);
 
     uint8_t led = 0b10000000;
-    setPin(ADDRESS, 0x0A, led);
+    setPin(ADDRESS, 0x0A, 0xFF);
 
     bool beat = false;
 
@@ -76,7 +108,7 @@ int main()
         
         beat = !beat;
         gpio_put(15, beat);
-        sleep_ms(100);
+        sleep_ms(500);
     }
 }
 
