e0814654a9f8a007567567b7bc0874482ec4d01a
Patrick McLean <patrickmclean2027@u.northwestern.edu>
2026-05-22 19:27:01 -0500

    x, y, z acc working, HB led turned off

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -39,18 +39,18 @@ int main()
     printf("hi!\n");
 
     sleep_ms(1000);
-
+    
     printf("trying to initialize ssd1306\n");
     ssd1306_setup();
     printf("ssd1306 initialized\n");
 
-    sleep_ms(1000);
+    //sleep_ms(1000);
 
     printf("trying to initialize mpu6050\n");
     mpu6050_setup();
     printf("mpu6050 initialized\n");
 
-    sleep_ms(1000);
+    //sleep_ms(1000);
 
     uint8_t identifier = mpu6050_whoami();
     printf("my name is 0x%02X\n", identifier);
@@ -68,12 +68,12 @@ int main()
     float measurements[7];
 
     while (true) {
-        gpio_put(HEART_BEAT, 1);
+        //gpio_put(HEART_BEAT, 1);
         mpu6050_collect_data(measurements);
 
         sprintf(message1, "X acc = %.3f g", measurements[0]);
         sprintf(message2, "Y acc = %.3f g", measurements[1]);
-        sprintf(message3, "Temp = %.3f K", measurements[3]);
+        sprintf(message3, "Z acc = %.3f g", measurements[2]);
 
         ssd1306_clear();
 
@@ -85,8 +85,8 @@ int main()
 
         sleep_ms(100);
 
-        gpio_put(HEART_BEAT, 0);
+        //gpio_put(HEART_BEAT, 0);
 
-        sleep_ms(100);
+        //sleep_ms(100);
     }
 }
